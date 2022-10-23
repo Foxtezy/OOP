dc3ddfdccaed9297c663b01c50a2b370a8630cd8
Kolacom <nmahov2003@gmail.com>
2022-10-23 23:04:49 +0700

    test \n

--- Task_1_3_1/lib/src/main/java/ru/nsu/fit/makhov/substring/SubstringFinder.java
+++ Task_1_3_1/lib/src/main/java/ru/nsu/fit/makhov/substring/SubstringFinder.java
@@ -3,6 +3,7 @@ package ru.nsu.fit.makhov.substring;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.nio.CharBuffer;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,18 +16,16 @@ public class SubstringFinder {
 
   private static final boolean NOT_SUBSTRING = false;
 
-  private static List<Integer> listSubstr;
+  private List<Integer> listSubstr;
 
-  private static char[] substring;
+  private char[] substring;
 
-  private static int countOfWritten = 0;
+  private int countOfWritten = 0;
 
-  private static boolean stat = NOT_SUBSTRING;
-  private static int subPointer = 0;
-  private static int numSubstring = 0;
+  private boolean stat = NOT_SUBSTRING;
+  private int subPointer = 0;
+  private int numSubstring = 0;
 
-  private SubstringFinder() {
-  }
 
   /**
    * Returns the indices of the beginning of a substring in a string.
@@ -34,14 +33,14 @@ public class SubstringFinder {
    * @param substring string to find
    * @return List of beginning substrings
    */
-  public static List<Integer> findSubstring(String fileName, String substring) {
-    SubstringFinder.listSubstr = new ArrayList<>();
-    SubstringFinder.substring = substring.toCharArray();
+  public List<Integer> findSubstring(String fileName, String substring) {
+    listSubstr = new ArrayList<>();
+    this.substring = substring.toCharArray();
     countOfWritten = 0;
     stat = NOT_SUBSTRING;
     subPointer = 0;
     numSubstring = 0;
-    char[] buff = new char[10];
+    CharBuffer buff = CharBuffer.allocate(100);
     try (Reader reader = new FileReader(fileName)) {
       while (reader.ready()) {
         int bufSize = reader.read(buff);
@@ -54,9 +53,29 @@ public class SubstringFinder {
     return listSubstr;
   }
 
-  private static void findSubstringInBuf(char[] buf, int bufSize) {
+  public List<Integer> findSubstring(Reader reader, String substring) {
+    listSubstr = new ArrayList<>();
+    this.substring = substring.toCharArray();
+    countOfWritten = 0;
+    stat = NOT_SUBSTRING;
+    subPointer = 0;
+    numSubstring = 0;
+    CharBuffer buff = CharBuffer.allocate(100);
+    try (reader) {
+      while (reader.ready()) {
+        int bufSize = reader.read(buff);
+        findSubstringInBuf(buff, bufSize);
+        countOfWritten += bufSize;
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return listSubstr;
+  }
+
+  private void findSubstringInBuf(CharBuffer buf, int bufSize) {
     for (int i = 0; i < bufSize; i++) {
-      if (buf[i] != substring[subPointer]) {
+      if (buf.get(i) != substring[subPointer]) {
         subPointer = 0;
         stat = NOT_SUBSTRING;
         continue;
@@ -64,7 +83,7 @@ public class SubstringFinder {
       if (stat == IN_SUBSTRING && subPointer < substring.length) {
         subPointer++;
       }
-      if (stat == NOT_SUBSTRING && (buf[i] == substring[subPointer])) {
+      if (stat == NOT_SUBSTRING && (buf.get(i) == substring[subPointer])) {
         numSubstring = i + countOfWritten;
         subPointer++;
         stat = IN_SUBSTRING;
--- Task_1_3_1/lib/src/test/java/ru/nsu/fit/makhov/substring/SubstringFinderTests.java
+++ Task_1_3_1/lib/src/test/java/ru/nsu/fit/makhov/substring/SubstringFinderTests.java
@@ -10,37 +10,39 @@ import org.junit.jupiter.api.Test;
 class SubstringFinderTests {
 
   private static String fileName;
+  private static SubstringFinder substringFinder;
 
   @BeforeAll
   public static void init() {
     fileName = "src/test/resources/input.txt";
+    substringFinder = new SubstringFinder();
   }
 
   @Test
   public void substringFinderOneCharTest() {
     Assertions.assertEquals(
         Collections.emptyList(),
-        SubstringFinder.findSubstring(fileName, "'"));
+        substringFinder.findSubstring(fileName, "'"));
     Assertions.assertEquals(Arrays.asList(0, 25, 51, 90, 116, 141),
-        SubstringFinder.findSubstring(fileName, "N"));
+        substringFinder.findSubstring(fileName, "N"));
     Assertions.assertEquals(
         Arrays.asList(19, 21, 43, 64, 70, 87, 109, 169, 175),
-        SubstringFinder.findSubstring(fileName, "u"));
+        substringFinder.findSubstring(fileName, "u"));
     Assertions.assertEquals(
         Arrays.asList(24, 50, 89, 115, 140),
-        SubstringFinder.findSubstring(fileName, "\n"));
+        substringFinder.findSubstring(fileName, "\n"));
   }
 
   @Test
   public void substringFinderWordTest() {
     Assertions.assertEquals(
         Arrays.asList(0, 25, 51, 90, 116, 141),
-        SubstringFinder.findSubstring(fileName, "Never gonna"));
+        substringFinder.findSubstring(fileName, "Never gonna"));
     Assertions.assertEquals(
         Arrays.asList(17, 41, 85, 107, 173),
-        SubstringFinder.findSubstring(fileName, "you"));
+        substringFinder.findSubstring(fileName, "you"));
     Assertions.assertEquals(
-        Collections.emptyList(), SubstringFinder.findSubstring(fileName,
+        Collections.emptyList(), substringFinder.findSubstring(fileName,
             "We're no strangers to love"));
   }
 }
