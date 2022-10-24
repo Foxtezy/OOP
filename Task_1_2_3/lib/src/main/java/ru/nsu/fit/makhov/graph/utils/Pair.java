27d8e660d9cf83b43b7592e3d00d86ca6757d1cf
Kolacom <nmahov2003@gmail.com>
2022-10-24 23:36:23 +0700

    update JDK to 17 version

--- Task_1_2_3/lib/src/main/java/ru/nsu/fit/makhov/graph/utils/Pair.java
+++ Task_1_2_3/lib/src/main/java/ru/nsu/fit/makhov/graph/utils/Pair.java
@@ -1,32 +1,11 @@
 package ru.nsu.fit.makhov.graph.utils;
 
-/**
- * Pair os some objects.
- *
- * @param <T> type of first object.
- * @param <E> type of second object.
- */
-public class Pair<T, E> {
-
-  private final T key;
-
-  private final E value;
-
-  public Pair(T key, E value) {
-    this.key = key;
-    this.value = value;
-  }
-
-  public T getKey() {
-    return key;
-  }
-
-  public E getValue() {
-    return value;
-  }
+public record Pair<T, E>(T key, E value) {
 
   @Override
   public String toString() {
-    return key + "(" + value + ')';
+    return key +
+        "(" + value +
+        ')';
   }
 }
