0ae79b5bebd31614792197d3eda5d942a92b6326
Foxtezy <Foxtezy@users.noreply.github.com>
2023-02-09 11:38:05 +0000

    Deploying to gh-pages from @ Foxtezy/OOP@c0157194bb3e4a3f98f0a1080b3cb593849e37fa 🚀

---
+++ Task_2_1_1/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.fit.makhov"},{"l":"ru.nsu.fit.makhov.multithread"},{"l":"ru.nsu.fit.makhov.parallelstream"},{"l":"ru.nsu.fit.makhov.singlethread"},{"l":"ru.nsu.fit.makhov.utils"}];updateSearchResults();
