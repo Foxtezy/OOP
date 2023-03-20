93b26007da24abf0a2102f2fd78cd33bf4573ad6
Николай Махов <nmahov2003@gmail.com>
2023-03-21 00:28:19 +0700

    init

---
+++ Task_2_3_1/lib/src/main/java/ru/nsu/fit/makhov/snake/JavaFXApplication.java
@@ -0,0 +1,30 @@
+package ru.nsu.fit.makhov.snake;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+public class JavaFXApplication extends Application {
+
+    private ConfigurableApplicationContext applicationContext;
+
+    @Override
+    public void init() {
+        applicationContext = new SpringApplicationBuilder(SpringApplication.class).run();
+    }
+
+    @Override
+    public void start(Stage stage) {
+        applicationContext.publishEvent(new StageReadyEvent(stage));
+    }
+
+    @Override
+    public void stop() {
+        this.applicationContext.close();
+        Platform.exit();
+    }
+}
---
+++ Task_2_3_1/lib/src/main/java/ru/nsu/fit/makhov/snake/SpringApplication.java
@@ -0,0 +1,10 @@
+package ru.nsu.fit.makhov.snake;
+
+import javafx.application.Application;
+
+public class SpringApplication {
+
+    public static void main(String[] args) {
+        Application.launch(JavaFXApplication.class, args);
+    }
+}
